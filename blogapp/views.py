e7e4e609a034c4d7072a1e39aa54a1f6eb7bbda0
Inder-PreetS <preetsingh0271@gmail.com>
2022-09-04 02:32:28 +0530

    latest code

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,4 @@
+from re import L
 from django.shortcuts import render,redirect
 from django.views.generic import View
 from .models import *
@@ -5,30 +6,6 @@ from .forms import *
 
 
 # Create your views here.
-def home(request):
-    AllBlogs=Blog.objects.all()
-    context={
-        'blogs':AllBlogs,
-    }
-    print(AllBlogs)
-    return render(request,'home.html',context)
-
-def addBlog(request):
-    form=blogForm()
-    if request.method=='POST':
-        print(request.POST, "======================", request.user)
-        post_values = request.POST.copy()
-        post_values['user.id']=request.user.id
-        print(post_values, "000000000000000000000000000000000000000000000000")
-
-        form=blogForm(request.POST,request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
-    context={
-        'form':form,
-    }
-    return render(request,'addblog.html',context)
 
 
 class IndexView(View):
@@ -39,4 +16,27 @@ class IndexView(View):
 class BlogDetailView(View):
     def get(self, request):
         return render(request, 'post-details.html')
+
+
+class AddBlogView(View):
+    def get(self, request):
+        form = blogForm()
+        return render(request, 'add-blog.html', {'form': form})
+# def AddBlog(request):
+#     form=blogForm()
+#     if request.method=='POST':
+#         print(request.POST, "======================", request.user)
+#         post_values = request.POST.copy()
+#         post_values['user.id']=request.user.id
+#         print(post_values, "000000000000000000000000000000000000000000000000")
+
+#         form=blogForm(request.POST,request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/')
+#     context={
+#         'form':form,
+#     }
+#     return render(request,'addblog.html',context)
+
 
